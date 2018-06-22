c43bd941a7d1ca92cc5240620d48113fd2bff784
Nicholas <nenglish443@outlook.com>
2018-06-21 23:30:19 -0400

    Update ExceptionInterface.php

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -2,17 +2,6 @@
 declare(strict_types=1);
 /**
  * Canary - A PSR-6 compliant cache library.
- *
- * @author Nicholas English <nenglish6657@gmail.com>.
- *
- * @license <https://github.com/Auvipev/Canary/blob/master/LICENSE> GNU General Public License v3.0.
- *
- * Permissions of this strong copyleft license are conditioned on making available complete
- * source code of licensed works and modifications, which include larger works using a licensed work,
- * under the same license. Copyright and license notices must be preserved. Contributors provide
- * an express grant of patent rights.
- *
- * @package Auvipev/Canary.
  */
 
 namespace Canary\Cache\Exception;
