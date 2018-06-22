f4aad6cb9114f2f2fbdc9c396317a947f1e22450
Nicholas <nenglish443@outlook.com>
2018-06-21 23:33:19 -0400

    Update DriverException.php

--- src/Exception/DriverException.php
+++ src/Exception/DriverException.php
@@ -1,1 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Canary - A PSR-6 compliant cache library.
+ */
 
+namespace Canary\Cache\Exception;
+
+/**
+ * The driver exception.
+ *
+ * @class      DriverException.
+ * @implements ExceptionInterface.
+ */
+class DriverException implements ExceptionInterface
+{
+}
