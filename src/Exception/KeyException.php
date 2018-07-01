810d488734558742962dd9bf89d10ea4ce4d199e
Nicholas <nenglish443@outlook.com>
2018-07-01 12:52:09 -0400

    Update KeyException.php

--- src/Exception/KeyException.php
+++ src/Exception/KeyException.php
@@ -2,6 +2,9 @@
 declare(strict_types=1);
 /**
  * Canary - A PSR-6 compliant cache library.
+ *
+ * @link <https://github.com/Auvipev/Canary> Github Repository.
+ * @link <https://www.php-fig.org/psr/psr-6> PSR-6: Caching Interface.
  */
 
 namespace Canary\Cache\Exception;
