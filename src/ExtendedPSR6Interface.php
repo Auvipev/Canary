3c95ce46157af78194751041e991070f02b4e2e6
Nicholas <nenglish443@outlook.com>
2018-07-05 03:58:53 -0400

    Update ExtendedPSR6Interface.php

--- src/ExtendedPSR6Interface.php
+++ src/ExtendedPSR6Interface.php
@@ -1,1 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Canary - A PSR-6 compliant cache library.
+ *
+ * @link <https://github.com/Auvipev/Canary> Github Repository.
+ * @link <https://www.php-fig.org/psr/psr-6> PSR-6: Caching Interface.
+ */
 
+namespace Canary\Cache;
+
+/**
+ * @interface ExtendedPSR6Interface.
+ */
+interface ExtendedPSR6Interface
+{
+}
